ad045ca11798e7232c8d33bbb52cce0abbaa5a47
GiuliaRomani <giulia.romani@mail.polimi.it>
2023-05-18 10:08:44 +0000

    Add other files

--- QuadratureNodes.hpp
+++ QuadratureNodes.hpp
@@ -1,4 +1,25 @@
+#ifndef QUADRATURENODES_HPP
+#define QUADRATURENODES_HPP
+
+#include <Eigen/Dense>
+
+#include "TypeTraits.hpp"
+
+using T = TypeTraits;
+
 namespace QuadratureNodes{
-    struct Points9;
-    struct Points10;
+    // QuadratureFormula with 9 points
+    struct Points9{
+        T::NumberType n = 9;
+
+    };
+
+    // QuadratureFormula with 10 points
+    struct Points10{
+        T::NumberType n = 10;
+
+    };
 };
+
+
+#endif //QUADRATURENODES_HPP
---
+++ TypeTraits.hpp
@@ -0,0 +1,7 @@
+#include <string>
+
+struct TypeTraits{
+    using NumberType = unsigned int;                // Type used to indicate a positive integer number of elements
+    
+
+};
